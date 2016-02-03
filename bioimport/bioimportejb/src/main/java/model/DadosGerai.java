7c293d1d4d4cf9e853b2895b3030601962858275
Maycon <Maycon@146.134.249.3>
2016-02-02 22:58:55 -0200

    Criação de novas entidades do novo modelo

---
+++ bioimport/bioimportejb/src/main/java/model/DadosGerai.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the dados_gerais database table.
+ * 
+ */
+@Entity
+@Table(name="dados_gerais", schema="bio")
+@NamedQuery(name="DadosGerai.findAll", query="SELECT d FROM DadosGerai d")
+public class DadosGerai implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_dados_gerais")
+	private Long idDadosGerais;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="dt_inclusao")
+	private Date dtInclusao;
+
+	private String valor;
+
+	//bi-directional many-to-one association to Analise
+	@ManyToOne
+	@JoinColumn(name="id_analise")
+	private Analise analise;
+
+	//bi-directional many-to-one association to DescritorUnidade
+	@ManyToOne
+	@JoinColumn(name="id_descritor_unidade")
+	private DescritorUnidade descritorUnidade;
+
+	//bi-directional many-to-one association to Usuario
+	@ManyToOne
+	@JoinColumn(name="id_usuario")
+	private Usuario usuario;
+
+	public DadosGerai() {
+	}
+
+	public Long getIdDadosGerais() {
+		return this.idDadosGerais;
+	}
+
+	public void setIdDadosGerais(Long idDadosGerais) {
+		this.idDadosGerais = idDadosGerais;
+	}
+
+	public Date getDtInclusao() {
+		return this.dtInclusao;
+	}
+
+	public void setDtInclusao(Date dtInclusao) {
+		this.dtInclusao = dtInclusao;
+	}
+
+	public String getValor() {
+		return this.valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public Analise getAnalise() {
+		return this.analise;
+	}
+
+	public void setAnalise(Analise analise) {
+		this.analise = analise;
+	}
+
+	public DescritorUnidade getDescritorUnidade() {
+		return this.descritorUnidade;
+	}
+
+	public void setDescritorUnidade(DescritorUnidade descritorUnidade) {
+		this.descritorUnidade = descritorUnidade;
+	}
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
---
+++ bioimport/bioimportejb/src/main/java/model/Taxon.java
@@ -0,0 +1,96 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the taxon database table.
+ * 
+ */
+@Entity
+@Table(name="taxon", schema="bio")
+@NamedQuery(name="Taxon.findAll", query="SELECT t FROM Taxon t")
+public class Taxon implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_taxon")
+	private Long idTaxon;
+
+	private String nome;
+
+	//bi-directional many-to-one association to AnaliseBio
+	@OneToMany(mappedBy="taxon")
+	private List<AnaliseBio> analiseBios;
+
+	//bi-directional many-to-one association to DadosTaxon
+	@ManyToOne
+	@JoinColumn(name="taxonkey")
+	private DadosTaxon dadosTaxon;
+
+	//bi-directional many-to-one association to Usuario
+	@ManyToOne
+	@JoinColumn(name="id_usuario")
+	private Usuario usuario;
+
+	public Taxon() {
+	}
+
+	public Long getIdTaxon() {
+		return this.idTaxon;
+	}
+
+	public void setIdTaxon(Long idTaxon) {
+		this.idTaxon = idTaxon;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<AnaliseBio> getAnaliseBios() {
+		return this.analiseBios;
+	}
+
+	public void setAnaliseBios(List<AnaliseBio> analiseBios) {
+		this.analiseBios = analiseBios;
+	}
+
+	public AnaliseBio addAnaliseBio(AnaliseBio analiseBio) {
+		getAnaliseBios().add(analiseBio);
+		analiseBio.setTaxon(this);
+
+		return analiseBio;
+	}
+
+	public AnaliseBio removeAnaliseBio(AnaliseBio analiseBio) {
+		getAnaliseBios().remove(analiseBio);
+		analiseBio.setTaxon(null);
+
+		return analiseBio;
+	}
+
+	public DadosTaxon getDadosTaxon() {
+		return this.dadosTaxon;
+	}
+
+	public void setDadosTaxon(DadosTaxon dadosTaxon) {
+		this.dadosTaxon = dadosTaxon;
+	}
+
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+}
